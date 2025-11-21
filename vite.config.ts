b5df0c02afc26bba47dda4cc4bfed5d581e8bbc6
Gonzalo Segura Taguas <gonsaliyo7@gmail.com>
2025-11-21 09:26:07 +0100

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Importante para que funcione en subdirectorios o dist
+  base: '/Gonzalo_Segura_CV/', // Base URL for GitHub Pages
   build: {
     outDir: 'dist', // La build se guardará en /dist (estándar de Vite)
     assetsDir: 'assets',
